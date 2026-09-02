1a9b69be45168b76f3e3d9b946fce568e50049bc
Zeenat Ansari <libraskingdom@gmail.com>
2026-09-02 16:58:03 +0530

    Add uniformArray method to check array parity

---
+++ Solutions/Construct Uniform Parity Array I.py
@@ -0,0 +1,4 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        # It is always possible to form an array with uniform parity (specifically, all even elements).
+        return True
